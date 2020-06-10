9dfe8e2461cfa9f368d69bf55f47579e4af501d6
jfederico <jesus@123it.ca>
2020-06-10 16:59:32 -0400

    added validation to sessions

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,9 +5,8 @@ class Api::V1::SessionsController < Api::V1::BaseController
 
   def validate_launch
     app_launch = AppLaunch.find_by_nonce(params[:token])
-
-    response = { token: params[:token], valid: true, message: JSON.parse(app_launch.message) }
-    render json: response.to_json
+    render json: { token: params[:token], valid: false }.to_json unless app_launch
+    render json: { token: params[:token], valid: true, message: JSON.parse(app_launch.message) }.to_json
   end
 
 end
