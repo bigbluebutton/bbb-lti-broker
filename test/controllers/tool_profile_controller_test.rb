7b64883d1c0be0b8156e07728ed78fe5a5cf243c
Billzzhang <billzhang345@gmail.com>
2020-06-25 15:54:49 -0400

    fix previous tests and added more tests

--- test/controllers/tool_profile_controller_test.rb
+++ test/controllers/tool_profile_controller_test.rb
@@ -3,14 +3,32 @@
 require 'test_helper'
 
 class ToolProfileControllerTest < ActionDispatch::IntegrationTest
-  test 'responds with xml_config for default with no parameters' do
+  test 'responds with xml_config for default with no parameters when developer mode is true' do
+    ENV["DEVELOPER_MODE_ENABLED"] = "true"
     get xml_config_path('default')
 
     # Response must be successful
     assert_response(:success)
 
     # Element blti:title should never be empty
+    
     doc = Nokogiri::XML(response.body)
     assert_not(doc.xpath('//blti:title').text.empty?)
+  end
+
+  test 'respond 404 for xml_config with developer mode is false' do
+    ENV["DEVELOPER_MODE_ENABLED"] = "false"
+    get xml_config_path('default')
+
+    # Response must be successful
+    assert_response(:missing)
+  end
+
+  test 'respond 404 for xml_builder with developer mode is false' do
+    ENV["DEVELOPER_MODE_ENABLED"] = "false"
+    get xml_builder_path('default')
+
+    # Response must be successful
+    assert_response(:missing)
   end
 end
