93b0eed5c3b62a847d9d8ff805608ac9a6debae4
Billzzhang <billzhang345@gmail.com>
2020-07-13 12:13:30 -0400

    rubocop changes

--- test/controllers/tool_profile_controller_test.rb
+++ test/controllers/tool_profile_controller_test.rb
@@ -31,14 +31,10 @@ class ToolProfileControllerTest < ActionDispatch::IntegrationTest
     # Response must be successful
     assert_response(:missing)
   end
-<<<<<<< HEAD
-
   test 'XML builder gives xml properties that are selected for cartridge link' do
     ENV['DEVELOPER_MODE_ENABLED'] = 'true'
-    get xml_config_path('default') + "?assignment_menu_message_type=content_item_selection&assignment_selection_message_type=content_item_selection_request&collaboration_message_type=content_item_selection_request&course_assignments_menu_message_type=basic_lti_request&discussion_topic_menu_message_type=content_item_selection&editor_button_message_type=content_item_selection_request&file_menu_message_type=content_item_selection&homework_submission_message_type=content_item_selection_request&link_selection_message_type=content_item_selection_request&migration_selection_message_type=content_item_selection_request&module_menu_message_type=content_item_selection&quiz_menu_message_type=content_item_selection&similarity_detection_message_type=basic_lti_request&wiki_page_menu_message_type=content_item_selection&selection_height=500&selection_width=500"
+    get xml_config_path('default') + '?selection_height=500&selection_width=500'
     page = Nokogiri::HTML.parse(@response.body)
     assert(page.xpath('//extensions/property'))
   end
-=======
->>>>>>> 779bcfd71e5bbe2284c431f24e4870829118bb33
 end
