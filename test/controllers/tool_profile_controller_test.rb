f9729ff90e29266ceece26f88ce6ca02a0891066
Bill Zhang <billzhang345@gmail.com>
2020-07-24 10:49:34 -0400

    LTI-20: Tool Profile not working (#22)
    * add check for developer mode
    
    * error page addition
    
    * add 404 page in non developer mode
    
    * clean up prints
    
    * Fixing routes
    
    * gitignore
    
    * included build_number in landing page (#19)
    
    * LTI-XX: Fixed script for building docker image on releases
    
    * LTI-XX: removed unnecessary directory
    
    * LTI-XX: Fix to issue carried from the major refactoring LTI-3 #44
    
    * LTI-30: fixed logs not visible though journald
    
    * LTI-5: Added a simple and basic health_check
    
    * gitignore
    
    * rubocop changes
    
    * fix previous tests and added more tests
    
    * rubocop tests
    
    * Testing to xml
    
    * rubocop changes
    
    Co-authored-by: Jesus Federico <jesus@123it.ca>

--- test/controllers/tool_profile_controller_test.rb
+++ test/controllers/tool_profile_controller_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require 'nokogiri'
 
 class ToolProfileControllerTest < ActionDispatch::IntegrationTest
   test 'responds with xml_config for default with no parameters when developer mode is true' do
@@ -29,5 +30,11 @@ class ToolProfileControllerTest < ActionDispatch::IntegrationTest
 
     # Response must be successful
     assert_response(:missing)
+  end
+  test 'XML builder gives xml properties that are selected for cartridge link' do
+    ENV['DEVELOPER_MODE_ENABLED'] = 'true'
+    get xml_config_path('default') + '?selection_height=500&selection_width=500'
+    page = Nokogiri::HTML.parse(@response.body)
+    assert(page.xpath('//extensions/property'))
   end
 end
