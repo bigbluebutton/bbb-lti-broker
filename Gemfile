ea508428dab85a3bd9addb7a55c59214f6e2def1
Bill Zhang <billzhang345@gmail.com>
2020-07-24 11:48:27 -0400

    Add and Use Rspec tests rather than rail minitest (#24)
    * add check for developer mode
    
    * error page addition
    
    * add 404 page in non developer mode
    
    * clean up prints
    
    * Fixing routes
    
    * gitignore
    
    * included build_number in landing page (#19)
    
    * LTI-XX: Fixed script for building docker image on releases
    
    * LTI-XX: removed unnecessary directory
    
    * LTI-XX: Fix to issue carried from the major refactoring LTI-3 #44
    
    * LTI-30: fixed logs not visible though journald
    
    * LTI-5: Added a simple and basic health_check
    
    * gitignore
    
    * rubocop changes
    
    * fix previous tests and added more tests
    
    * rubocop tests
    
    * added rspec tests with license text
    
    * changed travis to rubocop test instead
    
    * db host change
    
    * Update Gemfile.lock
    
    * remove bundle with
    
    * Update Gemfile.lock
    
    Co-authored-by: Jesus Federico <jesus@123it.ca>

--- Gemfile
+++ Gemfile
@@ -53,6 +53,8 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
   gem 'dotenv-rails'
+  gem 'rspec'
+  gem 'rspec-rails'
 end
 
 group :development do
--- test/controllers/tool_profile_controller_test.rb
+++ test/controllers/tool_profile_controller_test.rb
@@ -15,22 +15,6 @@ class ToolProfileControllerTest < ActionDispatch::IntegrationTest
     doc = Nokogiri::XML(response.body)
     assert_not(doc.xpath('//blti:title').text.empty?)
   end
-
-  test 'respond 404 for xml_config with developer mode is false' do
-    ENV['DEVELOPER_MODE_ENABLED'] = 'false'
-    get xml_config_path('default')
-
-    # Response must be successful
-    assert_response(:missing)
-  end
-
-  test 'respond 404 for xml_builder with developer mode is false' do
-    ENV['DEVELOPER_MODE_ENABLED'] = 'false'
-    get xml_builder_path('default')
-
-    # Response must be successful
-    assert_response(:missing)
-  end
   test 'XML builder gives xml properties that are selected for cartridge link' do
     ENV['DEVELOPER_MODE_ENABLED'] = 'true'
     get xml_config_path('default') + '?selection_height=500&selection_width=500'
