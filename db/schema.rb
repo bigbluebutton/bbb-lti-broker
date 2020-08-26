0bf02a2412f5d2c2371fce935cfae0b5be90641c
Bill Zhang <billzhang345@gmail.com>
2020-08-26 13:49:24 -0400

    LTI-80: Change the session_store from cookie_store to active_record_store (#40)
    * LTI-80: Change the session_store from cookie_store to active_record_store
    
    * rubocop clean up
    
    Co-authored-by: Jesus Federico <jesus@123it.ca>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_170829) do
+ActiveRecord::Schema.define(version: 2020_08_25_183836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,15 @@ ActiveRecord::Schema.define(version: 2020_06_05_170829) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string "lti_version"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
