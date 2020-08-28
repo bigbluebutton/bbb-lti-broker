7ec139bb58ba6440fd3e19ce0a426403feaec214
jfederico <jesus@123it.ca>
2020-08-28 10:46:53 -0400

    LTI-90: Removed unnecessary model

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -20,8 +20,8 @@ class Api::V1::SessionsController < Api::V1::BaseController
   before_action :doorkeeper_authorize!
 
   def validate_launch
-    app_launch = AppLaunch.find_by_nonce(params[:token])
-    render(json: { token: params[:token], valid: false }.to_json) unless app_launch
-    render(json: { token: params[:token], valid: true, message: JSON.parse(app_launch.message) }.to_json)
+    launch = RailsLti2Provider::LtiLaunch.find_by_nonce(params[:token])
+    render(json: { token: params[:token], valid: false }.to_json) unless launch
+    render(json: { token: params[:token], valid: true, message: launch.message }.to_json)
   end
 end
--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -21,19 +21,12 @@ class AppsController < ApplicationController
   # launch into lti application
   def launch
     # Make launch request to LTI-APP
-    redirector = "#{lti_app_url(params[:app])}?#{{ launch_nonce: app_launch.nonce }.to_query}"
+    launch = RailsLti2Provider::LtiLaunch.find_by(nonce: params[:oauth_nonce])
+    redirector = "#{lti_app_url(params[:app])}?#{{ launch_nonce: launch.nonce }.to_query}"
     redirect_to(redirector)
   end
 
   private
-
-  def app_launch
-    tool = RailsLti2Provider::Tool.where(uuid: params[:oauth_consumer_key]).last
-    lti_launch = RailsLti2Provider::LtiLaunch.find_by(nonce: params[:oauth_nonce])
-    AppLaunch.find_or_create_by(nonce: lti_launch.nonce) do |launch|
-      launch.update(tool_id: tool.id, message: standarized_message(lti_launch.message.to_json))
-    end
-  end
 
   def standarized_message(message_json)
     message = JSON.parse(message_json)
