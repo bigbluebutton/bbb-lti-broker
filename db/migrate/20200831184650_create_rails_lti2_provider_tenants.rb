0a49527c667299c3aaefb8c2258a7fc241ab71ce
jfederico <jesus@123it.ca>
2020-09-01 13:11:58 -0400

    fixed issues detected by rubocop

--- db/migrate/20200831184650_create_rails_lti2_provider_tenants.rb
+++ db/migrate/20200831184650_create_rails_lti2_provider_tenants.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class CreateRailsLti2ProviderTenants < ActiveRecord::Migration[6.0]
   def change
     create_table :rails_lti2_provider_tenants do |t|
-      t.string :uuid
+      t.string(:uuid)
 
       t.timestamps
     end
--- db/migrate/20200831193410_add_tool_association_to_tenant.rb
+++ db/migrate/20200831193410_add_tool_association_to_tenant.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class AddToolAssociationToTenant < ActiveRecord::Migration[4.2]
   def self.up
-    add_column :rails_lti2_provider_tools, :tenant_id, :integer
-    add_index 'rails_lti2_provider_tools', ['tenant_id'], :name => 'index_tenant_id'
+    add_column(:rails_lti2_provider_tools, :tenant_id, :integer)
+    add_index('rails_lti2_provider_tools', ['tenant_id'], name: 'index_tenant_id')
   end
 
   def self.down
-    remove_column :rails_lti2_provider_tools, :tenant_id
+    remove_column(:rails_lti2_provider_tools, :tenant_id)
   end
 end
