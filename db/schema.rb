dae534c3c6ae1460a8d1438ab39545d5cc544e43
jfederico <jesus@123it.ca>
2020-09-14 13:01:55 -0400

    restored schema.db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_160845) do
+ActiveRecord::Schema.define(version: 2020_08_25_183836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,8 +92,6 @@ ActiveRecord::Schema.define(version: 2020_09_14_160845) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string "lti_version"
-    t.integer "tenant_id"
-    t.index ["tenant_id"], name: "index_tenant_id"
   end
 
   create_table "sessions", force: :cascade do |t|
