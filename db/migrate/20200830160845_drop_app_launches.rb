b269d4d445d4fd7210774a66eaa16ffd1aa21c73
jfederico <jesus@123it.ca>
2020-09-14 13:13:40 -0400

    updated migrations

--- db/migrate/20200830160845_drop_app_launches.rb
+++ db/migrate/20200830160845_drop_app_launches.rb
@@ -2,6 +2,12 @@
 
 class DropAppLaunches < ActiveRecord::Migration[6.0]
   def change
-    drop_table :rails_lti2_provider_launches, if_exists: true
+    drop_table :app_launches do |t|
+      t.string(:tool_id)
+      t.string(:nonce)
+      t.text(:message)
+
+      t.timestamps
+    end
   end
 end
