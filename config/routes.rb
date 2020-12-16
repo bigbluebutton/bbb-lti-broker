877c0b93fb9efec7e8c8d6e43662ca64018d7ced
Billzzhang <billzhang345@gmail.com>
2020-12-16 16:54:23 -0500

    Working lti 1.3 deployments

--- config/routes.rb
+++ config/routes.rb
@@ -28,11 +28,16 @@ Rails.application.routes.draw do
   scope '/admin' do
     get '/', to: 'admin#home', as: :admin_home
     get '/users', to: 'admin#users', as: :admin_users
+
     get '/keys', to: 'admin#keys', as: :admin_keys
     post '/keys/submit', to: 'admin#submit_key', as: :admin_submit_key
     post '/keys/edit', to: 'admin#edit_key', as: :admin_edit_key
     post '/keys/delete', to: 'admin#delete_key', as: :admin_delete_key
+
     get '/deployments', to: 'admin#deployments', as: :admin_deployments
+    post '/deployments/submit', to: 'admin#submit_deployment', as: :admin_submit_deployment
+    post '/deployments/delete', to: 'admin#delete_deployment', as: :admin_delete_deployment
+
     get '/customization', to: 'admin#customization', as: :admin_customization
   end
 
