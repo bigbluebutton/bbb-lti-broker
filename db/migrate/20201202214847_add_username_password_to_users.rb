710630ec918ed2f376196a19c351791f4cd856af
jfederico <jesus@123it.ca>
2021-11-01 13:25:51 -0400

    ammended migration for passing rubocop

--- db/migrate/20201202214847_add_username_password_to_users.rb
+++ db/migrate/20201202214847_add_username_password_to_users.rb
@@ -2,9 +2,13 @@
 
 class AddUsernamePasswordToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column(:users, :username, :string)
-    add_column(:users, :password_digest, :string)
-    add_column(:users, :admin, :boolean)
+    change_table(:users, bulk: true) do |t|
+      t.string(:username)
+      t.string(:password_digest)
+      t.boolean(:admin)
+    end
     add_index(:users, :username, unique: true)
   end
 end
+
+change_table :users, bulk: true
