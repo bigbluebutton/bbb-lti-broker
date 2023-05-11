7eac562d064e06271a95209856fe49fadaf03103
Jesus Federico <jesus@123it.ca>
2023-05-11 10:29:33 -0400

    LTI-255: allows the overriding of params based on custom params (#150)
    * LTI-255: allows the overriding of params based on custom params
    
    * LTI-255: cleaned rubocops
    
    * reverted doorkeeper to 5.4

--- lib/bbb_lti_broker/helpers.rb
+++ lib/bbb_lti_broker/helpers.rb
@@ -67,7 +67,7 @@ module BbbLtiBroker
         message['resource_link_description'] = message['unknown_params']['https://purl.imsglobal.org/spec/lti/claim/resource_link']['description']
         message['launch_presentation_locale'] = message['unknown_params']['https://purl.imsglobal.org/spec/lti/claim/launch_presentation']['locale']
       end
-      message.to_json
+      custom_overrides(message).to_json
     end
 
     def user_params(tc_instance_guid, params)
@@ -85,6 +85,27 @@ module BbbLtiBroker
         last_name: params['lis_person_name_family'],
         last_accessed_at: Time.current,
       }
+    end
+
+    ##
+    # Overrides core parameters with custom parameters when following certain pattern.
+    #
+    # Core parameters may have to be overriden in order to make the applications behave differently
+    # for that, the LTI link in the tool consumer would need to include a custom parameter in the form:
+    #
+    # custom_resource_link_id=static:"some value" -> resource_link_id="some value"
+    # custom_resource_link_id=param:contenxt_id   -> resource_link_id=<value obtained from context_id>
+    # custom_resource_link_id="another value"     -> resource_link_id=<no overriding is made>
+    #
+    def custom_overrides(message)
+      custom_params = message['custom_params'].to_h
+      custom_params.each do |key, value|
+        custom_param = key.delete_prefix('custom_')
+        pattern = value.split(':')
+        message[custom_param] = pattern[1] if pattern[0] == 'static'
+        message[custom_param] = message[pattern[1]] if pattern[0] == 'param'
+      end
+      message
     end
   end
 end
