7dd4f4bbb640ce3f65f8a55b3393c73fd7cbb113
Mariam05 <tomariam@gmail.com>
2024-01-30 18:33:33 -0500

    replaced coveralls gem with coveralls_reborn

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,9 +16,6 @@
 # You should have received a copy of the GNU Lesser General Public License along
 # with BigBlueButton; if not, see <http://www.gnu.org/licenses/>.
 
-require 'coveralls'
-Coveralls.wear!
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
