035028502575f426b65d280f7689e8a9a974ae8f
Jesus Federico <jesus@123it.ca>
2024-03-25 09:56:10 -0400

    LTI-329: Add a mechanism to link a dynamic registration to a tenant (#208)
    * LTI-333: refactoring tenant rake task to handle registration_token through metadata
    
    * LTI-329: first pass to link a dynamic registration to a tenant
    
    * LTI-329: updated RailsLti2Provider gem
    
    * LTI-329: fixed rubocops
    
    * LTI-328: remove old keys on dynamic registration type 'reg-update'
    
    * LTI-333: rubocop clean up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_23_142223) do
+ActiveRecord::Schema.define(version: 2024_03_19_175531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2024_02_23_142223) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.jsonb "settings", default: {}, null: false
+    t.jsonb "metadata", default: {}, null: false
     t.index ["uid"], name: "index_tenant_uid", unique: true
   end
 
