0e67ff4ec1a33314ccad4cc1336f8bf982011b5c
Jesus Federico <jesus@123it.ca>
2024-03-26 17:02:50 -0400

    Update new_framework_defaults.rb

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -18,7 +18,6 @@
 
 # Be sure to restart your server when you modify this file.
 
-
 # This file contains migration options to ease your Rails 5.0 upgrade.
 
 # Read the Guide for Upgrading Ruby on Rails for more info on each option.
