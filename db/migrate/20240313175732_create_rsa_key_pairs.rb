dde8e3e504353405d62aeb8de8762182c0e78ba9
jfederico <jesus@123it.ca>
2024-04-03 08:10:05 -0400

    LTI-309: changed migration in prep for adding data migration

--- db/migrate/20240313175732_create_rsa_key_pairs.rb
+++ db/migrate/20240313175732_create_rsa_key_pairs.rb
@@ -1,5 +1,5 @@
 class CreateRsaKeyPairs < ActiveRecord::Migration[6.1]
-  def change
+  def self.up
     create_table :rsa_key_pairs do |t|
       t.text :private_key
       t.text :public_key
@@ -7,5 +7,11 @@ class CreateRsaKeyPairs < ActiveRecord::Migration[6.1]
 
       t.timestamps null:false, precision: 6
     end
+
+    # data migration.
+  end
+
+  def self.down
+    drop_table :rsa_key_pairs, if_exists: true
   end
 end
