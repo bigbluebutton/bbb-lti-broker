2968cbc04e6afcc1543358b88785e7a0c1881a3c
Jesus Federico <jesus@123it.ca>
2024-04-16 13:17:53 -0400

    LTI-351: replaced key_pair_id with key_pair_token on pub_keyset URL (#219)
    * LTI-351: replaced key_pair_id with key_pair_token on pub_keyset URL
    
    * LTI-351: fixed error in data migration
    
    * LTI-351: added indexes to rsa_key_pairs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_19_175531) do
+ActiveRecord::Schema.define(version: 2024_04_15_203259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +107,9 @@ ActiveRecord::Schema.define(version: 2024_03_19_175531) do
     t.text "public_key"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "token"
+    t.index ["id"], name: "index_rsa_key_pairs_on_id", unique: true
+    t.index ["token"], name: "index_rsa_key_pairs_on_token", unique: true
   end
 
   create_table "sessions", force: :cascade do |t|
